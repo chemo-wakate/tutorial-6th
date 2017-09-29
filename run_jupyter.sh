6dc19ec8fb69f66cea3370587892915bff11406b
kubor <rkubo.4w@gmail.com>
2017-09-29 18:51:22 +0900

    :memo: Fix typo

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -7,7 +7,7 @@
 #         -it \  # -t: tty（標準入力を受け取るのに必要）をコンテナのプロセスに割り当てる -i: 標準入力を待ち受け状態にする
 #         -p 8888:8888 \  # portを設定 ホスト側:コンテナ側 Jupyterがデフォルトで8888を割り当てるのでこれに合わせている
 #         -v ${CWD}:/home/jovyan/chemo \  # カレントディレクトリをコンテナにマウント
-#         -t chemowakate/tutorial-6th  # イメージを指定
+#         chemowakate/tutorial-6th  # イメージを指定
 # example:
 #    sh ./run_jupyter.sh
 #
@@ -20,5 +20,5 @@ docker run \
     -it \
     -p 8888:8888 \
     -v ${CWD}:/home/jovyan/chemo \
-    -t chemowakate/tutorial-6th
+    chemowakate/tutorial-6th
 
